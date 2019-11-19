0e11a39c27d791e8ed4752545cccfe06937d0be8
eurobob <rob@stoicdigital.com>
2019-11-19 19:20:06 +0200

    initial homepage styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Logo from "../assets/svg/logo.svg"
+import Logo from "../components/logo"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Stoic Digital" />
+    {/* <img src={Logo} alt="" /> */}
+    <div style={{ padding: "8vmin" }}>
+      <Logo style={{ height: "30vmin" }} />
+      <blockquote style={{ fontSize: "6vmin", marginTop: "2em" }}>
+        <p style={{ margin: 0 }}>
+          <span style={{ fontFamily: "georgia" }}>&ldquo;</span>It takes a lot
+          of hard work
+        </p>
+        <p>
+          &nbsp;&nbsp;to make something simple
+          <span style={{ fontFamily: "georgia" }}>&rdquo;</span>
+        </p>
+        <footer>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;— Steve Jobs</footer>
+      </blockquote>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div> */}
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
