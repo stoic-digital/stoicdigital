1e136b9d35553deb854b249677ee2709e50fcaa3
eurobob <rob@stoicdigital.com>
2020-03-25 17:24:21 +0000

    separation of sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,22 @@
 import React from "react"
-// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-// import Logo from "../assets/svg/logo.svg"
-import Logo from "../components/logo"
+import About from "../components/about"
 import SEO from "../components/seo"
+import Footer from "../components/footer"
+import Splash from "../components/splash"
+import Work from "../components/work"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Stoic Digital" />
-    {/* <img src={Logo} alt="" /> */}
-    <div style={{ padding: "8vmin" }}>
-      <Logo style={{ height: "30vmin" }} />
-      <blockquote style={{ fontSize: "6vmin", marginTop: "2em" }}>
-        <p style={{ margin: 0 }}>
-          <span style={{ fontFamily: "georgia" }}>&ldquo;</span>It takes a lot
-          of hard work
-        </p>
-        <p>
-          &nbsp;&nbsp;to make something simple
-          <span style={{ fontFamily: "georgia" }}>&rdquo;</span>
-        </p>
-        <footer>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;— Steve Jobs</footer>
-      </blockquote>
-    </div>
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div> */}
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Stoic Digital" />
+      <Splash />
+      <About />
+      <Work></Work>
+      <Footer />
+    </Layout>
+  )
+}
 
 export default IndexPage
